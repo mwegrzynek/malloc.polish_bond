3e8ab5e315fc1a1cafda9bc87c14a188e1b994d8
Michał Węgrzynek <michal.wegrzynek@malloc.com.pl>
2024-01-14 20:07:28 +0100

    Wyliczanie wartości obligacji

--- tests/test_bond_value.py
+++ tests/test_bond_value.py
@@ -6,19 +6,7 @@ import pytest
 import pandas as pd
 
 
-from malloc.polish_bond_value.utils import bond_value, bond_value_decimal
-
-
-def test_bond_value_decimal():
-    returns = [
-        Decimal("2.8"),
-        Decimal("4.65"),
-        Decimal("6.45"),
-        Decimal("15.65"),
-        Decimal("14.75")        
-    ]
-    
-    assert bond_value_decimal(returns, 12, dt.date(2019, 7, 31), dt.date(2024, 1, 14)) * 3 == Decimal("424.05")
+from malloc.polish_bond_value.calculations import bond_value, bond_value_decimal
 
 
 def test_bond_value_1():
