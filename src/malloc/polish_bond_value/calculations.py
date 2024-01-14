fc51c8c421ec2a3e488ed2af0780e16d7e01058f
Michał Węgrzynek <michal.wegrzynek@malloc.com.pl>
2024-01-14 20:06:35 +0100

    Wyliczanie wartości obligacji

--- src/malloc/polish_bond_value/calculations.py
+++ src/malloc/polish_bond_value/calculations.py
@@ -16,52 +16,46 @@ def bond_maturity_date(bond_name: str, purchase_date: dt.date) -> dt.date:
     return dt.datetime.strptime(bond_name[3:], "%m%y").replace(day=purchase_date.day).date()
 
 
-def bond_value_decimal(bond_returns: list[Decimal], period_length: int, purchase_date: dt.date, valuation_date: dt.date) -> Decimal:
-    """Calculate the value of a bond. bond_percentages is a DataFrame with columns:
-        - change_date: date of the change of the return
-        - return: return of the bond in percent
-    """    
+def bond_value_decimal(rates: list[Decimal], period_length: int, purchase_date: dt.date, valuation_date: dt.date) -> Decimal:
+    """Calculate the value of a bond. Rates is a list of rates in periods, period_length is a capitalization period in months"""    
     current_value = Decimal(100)
     current_date = purchase_date    
 
-    for bond_return in bond_returns:
+    for rate in rates:
         previous_date = current_date        
         current_date = current_date + relativedelta(months=period_length)
 
         if current_date > valuation_date:
             break
 
-        current_value = (current_value * (Decimal(100) + bond_return) / Decimal(100)).quantize(TWO_PLACES)
+        current_value = (current_value * (Decimal(100) + rate) / Decimal(100)).quantize(TWO_PLACES)
     
     # Compute last value    
     current_value = current_value + (
-        current_value * bond_return / Decimal(100) * 
+        current_value * rate / Decimal(100) * 
         Decimal((valuation_date - previous_date).days) / Decimal((current_date - previous_date).days)
     )
 
     return current_value.quantize(TWO_PLACES)
 
 
-def bond_value(bond_returns: list[Decimal], period_length: int, purchase_date: dt.date, valuation_date: dt.date) -> Decimal:
-    """Calculate the value of a bond. bond_percentages is a DataFrame with columns:
-        - change_date: date of the change of the return
-        - return: return of the bond in percent
-    """    
+def bond_value(rates: list[float], period_length: int, purchase_date: dt.date, valuation_date: dt.date) -> float:
+    """Calculate the value of a bond. Rates is a list of rates in periods, period_length is a capitalization period in months"""    
     current_value = 100
     current_date = purchase_date    
 
-    for bond_return in bond_returns:
+    for rate in rates:
         previous_date = current_date        
         current_date = current_date + relativedelta(months=period_length)
 
         if current_date > valuation_date:
             break
 
-        current_value = round(current_value * (100 + bond_return) / 100, 2)
+        current_value = round(current_value * (100 + rate) / 100, 2)
     
     # Compute last value    
     current_value = current_value + (
-        current_value * bond_return / 100 * 
+        current_value * rate / 100 * 
         (valuation_date - previous_date).days / (current_date - previous_date).days
     )
 
--- tests/test_bond_value.py
+++ tests/test_bond_value.py
@@ -21,7 +21,16 @@ def test_bond_value_decimal():
     assert bond_value_decimal(returns, 12, dt.date(2019, 7, 31), dt.date(2024, 1, 14)) * 3 == Decimal("424.05")
 
 
-def test_bond_value():
+def test_bond_value_1():
     returns = [2.8, 4.65, 6.45, 15.65, 14.75]    
     assert abs(bond_value(returns, 12, dt.date(2019, 7, 31), dt.date(2024, 1, 14)) * 3 - 424.05) < 0.01
 
+
+def test_bond_value_2():
+    returns = [2.0, 18.70]        
+    assert abs(bond_value(returns, 12, dt.date(2022, 3, 30), dt.date(2024, 1, 14)) * 25 - 2927.75) < 0.01
+
+
+def test_bond_value_3():
+    returns = [7.25]        
+    assert abs(bond_value(returns, 12, dt.date(2023, 10, 30), dt.date(2024, 1, 14)) * 155 - 15734.05) < 0.01
